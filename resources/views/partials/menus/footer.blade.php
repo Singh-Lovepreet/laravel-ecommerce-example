3223f022ec439d51125d801f80974cc8039448c4
singh-lovepreet <lovecod5@gmail.com>
2019-04-12 20:56:30 +0530

    new repo

--- resources/views/partials/menus/footer.blade.php
+++ resources/views/partials/menus/footer.blade.php
@@ -3,6 +3,6 @@
         @if ($menu_item->title === 'Follow Me:')
             <li>{{ $menu_item->title }}</li>
         @endif
-        <li><a href="{{ $menu_item->link() }}"><i class="fa {{ $menu_item->title }}"></i></a></li>
+        <li><a href=""><i class="fa {{ $menu_item->title }}"></i></a></li>
     @endforeach
 </ul>
